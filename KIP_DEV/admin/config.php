e4d479e3138ef2e8ee6f47b5e0f99579e808aa72
Zeeten <infogbrf@gmail.com>
2015-01-27 18:46:29 +0530

    Initial Version

---
+++ KIP_DEV/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/kip/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/kip/');
+
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kissmatinternational.com/kip/admin/');
+define('HTTPS_CATALOG', 'http://kissmatinternational.com/kip/');
+
+define('DIR_APPLICATION', 'C:\wamp\www\kip/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\kip/system/');
+define('DIR_DATABASE', 'C:\wamp\www\kip/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\kip/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\kip/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\kip/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\kip/image/');
+define('DIR_CACHE', 'C:\wamp\www\kip/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\kip/download/');
+define('DIR_LOGS', 'C:\wamp\www\kip/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\kip/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'kip');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ KIP_DEV/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/kip/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/kip/');
+
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\kip/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\kip/system/');
+define('DIR_DATABASE', 'C:\wamp\www\kip/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\kip/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\kip/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\kip/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\kip/image/');
+define('DIR_CACHE', 'C:\wamp\www\kip/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\kip/download/');
+define('DIR_LOGS', 'C:\wamp\www\kip/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'kip');
+define('DB_PREFIX', 'oc_');
+?>
